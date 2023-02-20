91e6b1f81113c47d4d5a44c0593ad861b6c79d66
myUsername <myemail@gmail.com>
2023-02-20 14:43:05 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
-#include <time.h>	
+#include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Disciption: 'Check for number if it is positive or negative'
+ * Return: always 0
+ */
 int main(void)
 {
 	int n;
@@ -18,7 +20,7 @@ int main(void)
 	{
 		printf("%d is zero\n", n);
 	}
-	else 
+else
 	{
 		printf("%d is negative\n", n);
 	}
