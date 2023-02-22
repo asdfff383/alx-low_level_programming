5356fa69ae108bc4c68bd937d8fbd6f1ec7c6d8e
myUsername <myemail@gmail.com>
2023-02-21 16:52:36 -0800

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print the sun of even fibonacci numbers
+ * less than 4000000.
+ * Return: nothing.
+ */
+
+int main(void)
+{
+	int i = 0;
+	long  j = 1, k = 2, sun = k;
+
+	while (k * j < 4000000)
+	{
+	k += j;
+	if (k % 2 == 0)
+	sun += k;
+	j = k - j;
+	++i;
+	}
+	printf("%ld\n", sun);
+	return (0);
+}
