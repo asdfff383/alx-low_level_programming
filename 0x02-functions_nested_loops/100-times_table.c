801f76f068f00b086c2755f32e07cbb2a22c11e1
myUsername <myemail@gmail.com>
2023-02-21 16:19:10 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -18,7 +18,7 @@ void print_times_table(int n)
 	_putchar('0');
 	for (mult = 1; mult <= n; mult++)
 	{
-	_putchar('0');
+	_putchar(',');
 	_putchar(' ');
 	prod = num * mult;
 	if (prod <= 99)
@@ -32,9 +32,9 @@ void print_times_table(int n)
 	}
 	else if (prod <= 99 && prod >= 10)
 	{
-	_putchar((prod / 100) + '0');
+	_putchar((prod / 10) + '0');
 	}
-	_putchar((prod / 100) + '0');
+	_putchar((prod % 10) + '0');
 	}
 	_putchar('\n');
 	}
