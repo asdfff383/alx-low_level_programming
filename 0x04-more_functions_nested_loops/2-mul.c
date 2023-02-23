9ac9337369c75d320afad2afa7a25fa31ce2aa19
myUsername <myemail@gmail.com>
2023-02-23 11:11:31 -0800

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - check if a charcter is a digit
+ * @x: The number to be checked
+ * @y: second number to mul x
+ * Return: the result multiple x param and y param
+ */
+
+int mul(int x, int y)
+
+{
+
+	return (x * y);
+
+}
