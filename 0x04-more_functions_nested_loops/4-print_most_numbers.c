89e6c316197049ae0746ca06dd4d083764f45df4
myUsername <myemail@gmail.com>
2023-02-23 11:33:15 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print the numbet since 0 to 9
- * Description: print number excluding 2 and 4
- * Return: numbers since 0 to 9
+ * print_most_numbers - print the numbers since 0 up to 9
+ * Description: Prints number excluding 2 and 4
+ * Return: The numbers since 0 up to 9
  */
 
 void print_most_numbers(void)
